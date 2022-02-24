c0b05b77f2de670a4534ca8213be2e54789992d1
Eva Houžvičková <houzvickova@icloud.com>
2022-02-24 13:15:19 +0100

    Project fuction ready

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 // tady je místo pro náš program
+
+
+
+function hazejKostkou() {
+    let zprava = document.querySelector('#zprava');
+    let kostka = document.querySelector('#kostka');
+    let hodnota = Math.floor(Math.random() * 6 + 1);
+
+    if (hodnota === 6) {
+        zprava.textContent = 'Hodil si 6. Vyhrál si!'
+    } else {
+        zprava.textContent = "Hodil si číslo " + hodnota + '. Zkus to znovu.'
+    }
+
+    kostka.src = 'obrazky/' + hodnota + '.png'
+}
